7bb9979f902b535a2bec0b534d1cc93947e13764
life--death <legend-chen@hotmail.com>
2019-07-23 22:46:29 -0700

    auto finish

--- auto-bat.py
+++ auto-bat.py
@@ -1,14 +1,14 @@
-def xml_out(req:int):
+def xml_out(req:int,address:str):
+    name = address.split("\\")[-1]
     filter_dict = {0:"write-action.pmc",1:"write-reg.pmc",2:"TCP-catch.pmc",3:"process-create.pmc"}
-    name = input("please input the location: ")
     new_file = open("C:\\address.bat","w")
     new_file.write("set PM=C:\\Users\\WDAGUtilityAccount\\Desktop\\ProcessMonitor\\procmon.exe\n")
     new_file.write("start %PM% /AcceptEula /quiet /minimized /backingfile C:\\" + name +".pml \n")
     new_file.write("%PM%  /waitforidle /AcceptEula\n")
-    new_file.write("start /wait "+ name + "\n")
+    new_file.write("start /wait "+ address + "\n")
     new_file.write("%PM% /terminate /AcceptEula \n")
     for temp in range(4):
         if req[temp] == 1:
-            new_file.write("%PM% /SaveApplyFilter  /SaveAs1 C:\\" + name +".xml " + "/Openlog C:\\" + name + ".pml " +  "/quiet /minimized /AcceptEula /LoadConfig C:\\Users\\WDAGUtilityAccount\\Desktop\\"  + filter_dict[temp]  " \n")
+            new_file.write("%PM% /SaveApplyFilter  /SaveAs C:\\" + name + filter_dict[temp] +".csv " + "/Openlog C:\\" + name + ".pml " +  "/quiet /minimized /AcceptEula /LoadConfig C:\\Users\\WDAGUtilityAccount\\Desktop\\"  + filter_dict[temp]  " \n")
             new_file.write("%PM% /minimized /terminate /AcceptEula \n")
             new_file.close() 
